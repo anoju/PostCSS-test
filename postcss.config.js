7a65ad0a7720ccff6a1e02370ef3ade3994384e0
anoju <zun5761@gmail.com>
2023-03-13 17:38:03 +0900

    post Css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,29 @@
 module.exports = {
   plugins: [
+    require('postcss-assets')({
+      loadPaths: ['images/'],
+      relativeTo: 'css/',
+      cachebuster: true
+    }),
+    require('postcss-sprites')({
+      stylesheetPath: './pcss',
+      spritePath: './images/sprite',
+      retina: true
+    }),
+    require('postcss-import'),
+    require('postcss-inline-svg')(),
+    // postcss-nested 쓸바엔 SCSS?
+    require('postcss-nested'),
     require('autoprefixer'),
-    require('cssnano')({preset: 'default'})
-  ]
-}
+    // postcss-preset-env 역할을 잘 모르겠음
+    require('postcss-preset-env')({
+      stage: 3,
+      features: {
+        'nesting-rules': true,
+        'custom-selectors': { preserve: true }
+      }
+    }),
+    require('cssnano')({ preset: 'default' })
+  ],
+  sourceMap: true
+};
